e2ff17118ab6b0639da3a5d8613ae51f64e708d3
Denis Neacsa <neacsa.denis@yahoo.com>
2026-04-22 23:31:44 +0300

    chore: rename ridingMode attribute

--- src/com/pao/project/auction_app/models/vehicles/motorcycles/Naked.java
+++ src/com/pao/project/auction_app/models/vehicles/motorcycles/Naked.java
@@ -6,16 +6,16 @@ import com.pao.project.auction_app.models.vehicles.motorcycles.enums.RidingMode;
 
 public class Naked extends Motorcycle {
     private final boolean isStreetFighter;
-    private final RidingMode ridingMode[];
+    private final RidingMode ridingModes[];
     private final HeadlightType headlightType;
 
     public Naked(String manufacturer, String model, int year, int price, int mileage, Engine engine,
             Double engineCapacity, Double weight, boolean hasABS, boolean isA2Compatible, int numberOfCylinders,
-            boolean isStreetFighter, RidingMode[] ridingMode, HeadlightType headlightType) {
+            boolean isStreetFighter, RidingMode[] ridingModes, HeadlightType headlightType) {
         super(manufacturer, model, year, price, mileage, engine, engineCapacity, weight, hasABS, isA2Compatible,
                 numberOfCylinders);
         this.isStreetFighter = isStreetFighter;
-        this.ridingMode = ridingMode;
+        this.ridingModes = ridingModes;
         this.headlightType = headlightType;
     }
 
@@ -23,8 +23,8 @@ public class Naked extends Motorcycle {
         return isStreetFighter;
     }
 
-    public RidingMode[] getRidingMode() {
-        return ridingMode;
+    public RidingMode[] getRidingModes() {
+        return ridingModes;
     }
 
     public HeadlightType getHeadlightType() {
