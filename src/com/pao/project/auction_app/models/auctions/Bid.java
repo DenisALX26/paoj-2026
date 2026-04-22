3c588445d26e7a1db67a58b35411910a6975b169
Denis Neacsa <neacsa.denis@yahoo.com>
2026-04-22 13:37:06 +0300

    feat: implement getter for bidder balance

--- src/com/pao/project/auction_app/models/auctions/Bid.java
+++ src/com/pao/project/auction_app/models/auctions/Bid.java
@@ -3,15 +3,19 @@ package com.pao.project.auction_app.models.auctions;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import com.pao.project.auction_app.models.users.Bidder;
+import com.pao.project.auction_app.services.UserService;
+
 public final class Bid {
     private final UUID bidderId;
     private final double amount;
     private final LocalDateTime timestamp;
+    private final UserService userService = UserService.getInstance();
     
-    public Bid(UUID bidderId, double amount, LocalDateTime timestamp) {
+    public Bid(UUID bidderId, double amount) {
         this.bidderId = bidderId;
         this.amount = amount;
-        this.timestamp = timestamp;
+        this.timestamp = LocalDateTime.now();
     }
 
     public UUID getBidderId() {
@@ -29,6 +33,10 @@ public final class Bid {
     @Override
     public String toString() {
         return String.format("%.2f EUR la data %s", amount, timestamp);
+    }
+
+    public double getBidderBalance() {
+        return ((Bidder) userService.getUserById(bidderId)).getBalance();
     }
 
     
